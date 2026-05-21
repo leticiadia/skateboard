62e3f1af4f29d03f8b1e4cc760344217d49555a2
Leticia Dias <contatoleticiadia@gmail.com>
2026-05-21 15:51:15 -0300

    feat: add section athletes

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,15 @@
 import { SectionAbout } from "./sections/sectionAbout";
+import { SectionAthletes } from "./sections/sectionAthletes";
 import { SectionMain } from "./sections/sectionMain";
+
+import { athletes } from "./mocks/athletes";
 
 export function Home() {
   return (
     <>
       <SectionMain />
       <SectionAbout />
+      <SectionAthletes athletes={athletes} />
     </>
   );
 }
