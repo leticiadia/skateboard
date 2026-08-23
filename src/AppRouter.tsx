9c276a47074f81ec3a00d3fdca40b0ad94384bfc
Leticia Dias <contatoleticiadia@gmail.com>
2026-08-22 22:24:25 -0300

    feat(routes): add route to the athlete profile page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { About } from "./pages/About";
 import { Athletes } from "./pages/Athletes/Athletes";
+import { Championships } from "./pages/Championships/Championships";
+import { AthleteProfile } from "./pages/Athletes/AthleteProfile";
 
 export function AppRouter() {
   return (
@@ -9,6 +11,8 @@ export function AppRouter() {
       <Route path="/" element={<Home />}></Route>
       <Route path="/sobre" element={<About />}></Route>
       <Route path="/atletas" element={<Athletes />}></Route>
+      <Route path="/atletas/:slug" element={<AthleteProfile />}></Route>
+      <Route path="/campeonatos" element={<Championships />}></Route>
     </Routes>
   );
 }
