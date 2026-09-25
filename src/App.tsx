e79634c7d7a3fff6031b47f2516de758778babc1
Leticia Dias <contatoleticiadia@gmail.com>
2026-09-25 10:16:50 -0300

    fix(router): add ScrollToTop component to reset window scroll on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./AppRouter";
 import { Header } from "./components/layout/header/Header";
 import { Footer } from "./components/layout/footer/footer";
+import { ScrollToTop } from "./components/layout/ScrollToTop/ScrollToTop";
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
+
       <div className="flex flex-col min-h-screen w-full">
         <Header />
 
